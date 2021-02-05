843f74f2d4c1aecb59da899e793f1fa3c832e7b6
OOF2510 <gamekid01234@gmail.com>
2021-02-05 15:19:58 -0500

    allow sending death command as console, also fixed some bugs

--- src/main/java/me/nate/deathmessagecount/commands/DeathsCmd.java
+++ src/main/java/me/nate/deathmessagecount/commands/DeathsCmd.java
@@ -5,21 +5,27 @@ import org.bukkit.Statistic;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 
 public class DeathsCmd implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(args.length <= 1) {
+        if(args.length == 1) {
             String ps = args[0];
             Player p = Bukkit.getServer().getPlayer(ps);
             String pn = p.getName();
-            Player e = (Player) sender;
             int c = p.getStatistic(Statistic.DEATHS);
             String cs = String.valueOf(c);
             String n = pn+" has "+cs+" deaths";
-            e.sendMessage(n);
+            if (sender instanceof Player) {
+                Player e = (Player) sender;
+                e.sendMessage(n);
+            } else {
+                ConsoleCommandSender ccs = Bukkit.getServer().getConsoleSender();
+                ccs.sendMessage(n);
+            }
         } else {
             Player p = (Player) sender;
             int c = p.getStatistic(Statistic.DEATHS);
